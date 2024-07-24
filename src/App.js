1a94d509c2192886fba0ff7cba2e2a5b49048018
har1nagle <hnagle.0@yahoo.com>
2024-07-24 11:28:51 +0530

    add style numbering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import "./index.css";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
+
+  const step = 1;
+
+  function handlePrevious() {
+    alert("previous")
+  }
+
+   function handleNext() {
+     alert("Next");
+   }
+
   return (
-    <>
-    </>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
   );
 }
 
